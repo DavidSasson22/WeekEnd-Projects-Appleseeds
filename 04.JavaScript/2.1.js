00ba25de0945a5d9aaa5a0dffa9a252996301bb8
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-02-18 10:52:56 +0200

    2.2

---
+++ 04.JavaScript/2.1.js
@@ -0,0 +1,20 @@
+const numbers1 = [19, 5, 42, 2, 77];
+const numbers2 = [1, 2, 5, 8];
+
+
+const SumLowest = (array) => {
+  result = 0;
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < array.length - 1; j++) {
+      if (array[j] > 0 && array[j] < array[j + 1]) {
+        [array[j + 1], array[j]] = [array[j], array[j + 1]];
+      };
+    };
+    result += array.pop();
+  };
+  console.log(result);
+  return result
+};
+
+SumLowest(numbers1);
+SumLowest(numbers2);
---
+++ 04.JavaScript/2.2.js
@@ -0,0 +1,7 @@
+const test1 = [0, 0, 0, 1]
+const test2 = [0, 1, 0, 1]
+const test3 = [1, 0, 1, 1]
+
+const BinaryInt = (array) => {
+
+}
