0686dc1670e4d3766afc4ea621cb0e1eb1eeaecc
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-02-18 15:16:51 +0200

    Create 4.1.js

---
+++ 04.JavaScript/4.1.js
@@ -0,0 +1,9 @@
+const fibonacci = (num) => {
+  let result = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    result.push(result[result.length -1] + result[result.length -2]);
+  };
+  return result
+};
+
+console.log(fibonacci(10));
