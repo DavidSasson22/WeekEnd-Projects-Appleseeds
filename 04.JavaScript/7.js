36f0ce38eb39899f2b3c635ab2d594e9f37d6e7d
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-02-19 14:14:03 +0200

    Update 7.js

--- 04.JavaScript/7.js
+++ 04.JavaScript/7.js
@@ -5,13 +5,34 @@ const greaterThan10 = (num) => {
   return num > 10 ? true : false
 }
 
+/*-----------------FILTER-----------------*/
+
 const filter0 = (array, condition) => {
-  result = [];
+  let result = [];
   for (element of array) {
     condition(element) && result.push(element);
   }
   return result
 }
 
-console.log(7 > 6);
-console.log(filter0(test1, greaterThan10));
+
+
+
+/*-----------ForEach-------------*/
+
+const forEach0 = (array, someFunction) => {
+  for (let i = 0; i < array.length; i++) {
+    someFunction(array[i], i, array);
+  }
+}
+
+
+
+/*----------------Map----------------*/
+
+const map0 = (array, someFunction) => {
+  let copy = [];
+  for (let i = 0; i < array.length; i++) {
+    copy.push(someFunction(i), i, array);
+  }
+} 
